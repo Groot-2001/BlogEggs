5c10b284308d89bdcf726287ea69574dc092770c
Groot-2001 <shivasilmawala841@gmail.com>
2024-05-31 22:52:26 +0530

    testing exec operations in cache file

--- services/cache.js
+++ services/cache.js
@@ -4,5 +4,13 @@ const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("I'm About to Run a Query");
+  console.log(this.getFilter());
+  console.log(this.mongooseCollection.name);
+  const newObj = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+
+  console.log(newObj);
+
   return exec.apply(this, arguments);
 };
