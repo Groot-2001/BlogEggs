38af7328f2bbe20c78675b9074dd6c66a220775f
Groot-2001 <shivasilmawala841@gmail.com>
2024-06-02 22:09:29 +0530

    added redis configuration inside the cache implementation

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,17 @@
 const mongoose = require("mongoose");
+const redis = require("redis");
+const util = require("util");
+
+//setup redis configuration
+const redisUrl = "redis://127.0.0.1:6379";
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
   console.log("I'm About to Run a Query");
-  console.log(this.getFilter());
-  console.log(this.mongooseCollection.name);
+
   const newObj = Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name,
   });
